54d59b27b2383882ed413d3111b88a756875554e
Lương Quang Huy <lqhuy2309@gmail.com>
2021-04-24 22:27:40 +0700

    login

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.form_signup);
+        setContentView(R.layout.form_login);
 
 
     }
